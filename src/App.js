95e24c8e9f96fa7feff132abcea1a05821b6b190
davide.patti <davide.patti@autotrader.co.uk>
2018-10-27 16:51:42 +0100

    #1 Converted postcode to lat/long

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PostcodeInput from "./postcode/postcode.input";
 import PointComponent from './PointComponent'
 
 class App extends Component {
-
   render() {
-
     return (
       <div className="App">
+          <PostcodeInput />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <div>
---
+++ src/postcode/postcode.input.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import postcodesioClient from 'postcodesio-client'
+import './../App.css';
+
+
+class PostcodeInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            postcode: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let postcode = new postcodesioClient()
+
+        postcode.lookup(this.state.postcode).then(postcode => {
+            console.log(postcode);
+        })
+    }
+
+
+    handleChange(event) {
+        this.setState({postcode: event.target.value});
+    }
+
+    render () {
+
+        let submitButton
+        if(this.state.postcode === "" ){
+            submitButton = <input disabled={true} type="submit" value="Submit" onClick={this.handleSubmit}/>
+        }
+        else{
+            submitButton = <input type="submit" value="Submit" onClick={this.handleSubmit}/>
+        }
+
+        return <form>
+            <label>
+                Postcode:
+                <input type="text"
+                       className="App-postocde"
+                       value={this.state.postcode}
+                       placeholder="Insert a postcode"
+                       onChange={this.handleChange}
+                       />
+            </label>
+            {
+                submitButton
+            }
+        </form>
+    }
+}
+
+export default PostcodeInput
+
+
