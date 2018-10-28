1ca18695242cce75bd602c6f2ff82a324b95e0d0
David Carter <david.carter@autotrader.co.uk>
2018-10-28 09:27:03 +0000

    swipe for link

--- src/crime/PointComponent.js
+++ src/crime/PointComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactSwipeEvents from 'react-swipe-events'
 import rbush from 'rbush';
 import knn from 'rbush-knn';
 import fetch from 'isomorphic-fetch'
@@ -11,6 +12,9 @@ import Gauge from 'react-svg-gauge';
 class PointComponent extends Component {
     state = {
         nearestSpacesWithScores: false,
+        swipeDist0: 0,
+        swipeDist1: 0,
+        swipeDist2: 0,
     }
     
     async componentDidUpdate(prevProps) {
@@ -58,6 +62,12 @@ class PointComponent extends Component {
         }
     }
 
+    setSwipeDist = (dist, i) => {
+        this.setState({ [`swipeDist${i}`]: dist })
+    }
+
+    doGoogNav = streetName => {}
+
     render() {
         const { nearestSpacesWithScores } = this.state
         const { setSelectedLatLongDestination, selectedLatLongDestination } = this.props
@@ -65,32 +75,45 @@ class PointComponent extends Component {
         return (
             <Form>
             <FormGroup>
-                 { nearestSpacesWithScores && nearestSpacesWithScores.map((space, i) => 
-                 <span key={space.name}>
-                    <Radio name="radioGroup" 
-                        id={space.name}
-                        className='hidden'
-                        onChange={() => setSelectedLatLongDestination(space.latLong)}
-                    />
-                    <label htmlFor={space.name} className={`${selectedLatLongDestination === space.latLong && 'selected'} parking-result`}>
-                        <h3>{space.name}</h3>
-                        <p>Distance: {this.props.distanceBetweenOriginDestination[i]} meters</p>
-                        <span style={{float:'right', 'margin-top':'-50px'}}>
-                            <Gauge label='' 
-                                height={50} 
-                                width={60} 
-                                value={to2dp((convertToDisplayScore(space.score)/40) * 100)}
-                                color={getDialColourForValue(convertToDisplayScore(space.score))}
-                                minMaxLabelStyle={{display: 'none'}}
-                                backgroundColor='#000000'
+                { nearestSpacesWithScores && nearestSpacesWithScores.map((space, i) => 
+                    <ReactSwipeEvents
+                        key={i}
+                        onSwiping={(e, originalX, originalY, currentX, currentY, deltaX, deltaY) => 
+                            this.setSwipeDist(deltaX, i)
+                        }
+                        onSwiped={(e, originalX, originalY, endX, endY, deltaX, deltaY) => {
+                            this.setSwipeDist(0, i)
+                        }}>
+                        { this.state[`swipeDist${i}`] > -50 ?
+                            <span>
+                                <Radio name="radioGroup" 
+                                    id={space.name}
+                                    className='hidden'
+                                    key={space.name} 
+                                    onChange={() => setSelectedLatLongDestination(space.latLong)}
                                 />
-                        </span>
-                    </label>
-                        
-                 </span>
-                    
-                    
-                 ) }
+                                <label htmlFor={space.name}
+                                className={`${selectedLatLongDestination === space.latLong && 'selected'} parking-result`}>
+                                    <h3>{space.name}</h3>
+                                    <p>Distance: {this.props.distanceBetweenOriginDestination[i]} meters</p>
+                                    <span style={{float:'right', 'margin-top':'-50px'}}>
+                                        <Gauge label='' 
+                                            height={50} 
+                                            width={60} 
+                                            value={to2dp((convertToDisplayScore(space.score)/40) * 100)}
+                                            color={getDialColourForValue(convertToDisplayScore(space.score))}
+                                            minMaxLabelStyle={{display: 'none'}}
+                                            backgroundColor='#000000'
+                                            />
+                                    </span>
+                                </label>    
+                            </span> : 
+                            <a className='parking-result link-thing'
+                                href={`https://www.google.com/maps/dir/?api=1&${encodeURIComponent(space.name)}`}>{`Get directions to ${space.name} =>`}
+                            </a>
+                        }
+                    </ReactSwipeEvents>
+                )}
               </FormGroup>
             </Form>
         )
